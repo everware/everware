2026809bdb59a32e29415d8d4ab9877e92a5983f
Tim Head <betatim@gmail.com>
2016-03-06 12:09:17 +0100

    Find the log

--- build_tools/travis/test_frontend.sh
+++ build_tools/travis/test_frontend.sh
@@ -4,16 +4,17 @@
 # The tests run by this script are "frontend" testing
 
 set -e
+echo "In" `pwd`
 
 # Start a hub that our tests can interact with
 echo "Starting everware"
 jupyterhub -f build_tools/travis/frontend_test_config.py \
-           --no-ssl --debug > hub.txt 2>&1 &
+           --no-ssl --debug > /tmp/hub.txt 2>&1 &
 sleep 3
 
 echo "Start running frontend tests"
 nose2 -v --start-dir frontend_tests
 
 echo ">>> Logging output of jupyterhub"
-cat hub.log
+cat /tmp/hub.log
 echo "<<< Logging output of jupyterhub"
