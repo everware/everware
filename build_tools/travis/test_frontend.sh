84a368f80b42e52888ce88a4035a9835d08044d1
Tim Head <betatim@gmail.com>
2016-03-06 12:17:28 +0100

    We don't need logs

--- build_tools/travis/test_frontend.sh
+++ build_tools/travis/test_frontend.sh
@@ -9,12 +9,8 @@ echo "In" `pwd`
 # Start a hub that our tests can interact with
 echo "Starting everware"
 jupyterhub -f build_tools/travis/frontend_test_config.py \
-           --no-ssl --debug > /tmp/hub.txt 2>&1 &
+           --no-ssl --debug &
 sleep 3
 
 echo "Start running frontend tests"
 nose2 -v --start-dir frontend_tests
-
-echo ">>> Logging output of jupyterhub"
-cat /tmp/hub.log
-echo "<<< Logging output of jupyterhub"
