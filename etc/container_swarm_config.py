8508be46914a1ea043cfb1c50f1a762edab9b397
Bertrand NOEL <bertrand.noel.88@gmail.com>
2016-04-27 16:00:08 +0200

    Config and env file to run on Docker Swarm

---
+++ etc/container_swarm_config.py
@@ -0,0 +1,21 @@
+# Use this config file to run everware in a container,
+# and create the user container on a Docker Swarm cluster
+#
+# No need to mount /var/run/docker.sock,
+# but don't forget to set Docker Swarm environment
+# variables in the env file
+
+c = get_config()
+load_subconfig('etc/base_config.py')
+load_subconfig('etc/github_auth.py')
+
+c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.proxy_api_ip = '0.0.0.0'
+
+c.JupyterHub.spawner_class = 'everware.CustomSwarmSpawner'
+c.Spawner.tls = True
+c.DockerSpawner.tls = True
+
+# Change this setting:
+# IP of the machine where the Everware can be contacted
+c.DockerSpawner.hub_ip_connect = 'xxx.xxx.xxx.xxx'
