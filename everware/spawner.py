73b7db4bb8e52f1f104bf1aef1a544b0874b44a9
IgorMusinov <igor.musinov@phystech.edu>
2016-09-19 00:16:13 +0300

    f

--- everware/spawner.py
+++ everware/spawner.py
@@ -22,7 +22,7 @@ import os
 from .image_handler import ImageHandler
 from .git_processor import GitMixin
 from .email_notificator import EmailNotificator
-from .__init__ import __version__
+from . import version
 
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -413,7 +413,7 @@ class CustomDockerSpawner(DockerSpawner, GitMixin, EmailNotificator):
             env.update({
                 'JPY_GITHUBURL': self.repo_url_with_token,
                 'JPY_REPOPOINTER': self.commit_sha,
-                'EVER_VERSION':__version__,
+                'EVER_VERSION': version,
             })
         return env
 
