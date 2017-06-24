92835dba646da6e4f9722b34c4e3805299285da3
Yuriy Gorishniy <strausmg@gmail.com>
2017-06-25 01:09:58 +0300

    fix BYOR config

--- etc/byor_config.py
+++ etc/byor_config.py
@@ -3,3 +3,7 @@ load_subconfig('etc/base_config.py')
 load_subconfig('etc/github_auth.py')
 
 c.JupyterHub.spawner_class = 'everware.ByorDockerSpawner'
+
+from os.path import join as pjoin
+with open(pjoin(c.JupyterHub.template_paths[0], '_byor_options_form.html')) as form:
+    c.CustomDockerSpawner.options_form = form.read()
